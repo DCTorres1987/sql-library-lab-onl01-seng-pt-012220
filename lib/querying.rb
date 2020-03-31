a4e1fcc1ac82b66742b74a65d5e5343051301dbd
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-31 04:09:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT * FROM authors 
    INNER JOIN series ON 
-   authors.series_id = series.id"
+   authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
