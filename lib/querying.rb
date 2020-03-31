317e5b0091be1a2485757b138d6bd98d3f74e36b
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-31 04:23:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT  FROM characters
+  "SELECT characters.name, COUNT( FROM characters
   INNER JOIN character_books ON 
   characters.character_id = character_books.character_id
   JOIN books ON
