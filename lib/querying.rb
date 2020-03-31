62208f8d0e3e63679a7f954606fb5e6580d6f940
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-31 04:17:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,9 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
-  WHERE "
+   INNER JOIN characters ON 
+   series.author_id = character.author_id 
+   WHERE species = 'human' ORDER BY LENGTH(title) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
