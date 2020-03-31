4cb305a8dd861b66a0ad4811bbd7b94aba1e9b10
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-31 03:51:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books ORDER BY year ASC;"
+  "SELECT title, year FROM books 
+  INNER JOIN series ON 
+  books.series_id = series.id 
+  WHERE series.id = '1'
+  ORDER BY year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
